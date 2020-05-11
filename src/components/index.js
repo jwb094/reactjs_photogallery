2c73bbd83c2b902bbd5111e58122f9af27f88f39
Josh Bello <joshbello@joshs-mbp.home>
2020-05-11 20:01:29 +0100

    finished working on the api call &&  now need to make sure that i comply w unsplash api rules for displaying photos

--- src/components/index.js
+++ src/components/index.js
@@ -1,63 +1,67 @@
 import React, { Component } from 'react';
 
 class Index extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pics: []
+    };
+  }
 
-        };
-        }
 
 
-    
-            
-        /*componentDidMount() {
-          fetch(`https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_CLIENT_ID_KEY}`)
-            .then(result => result.json())
-            .then(result => {
-              console.log(result)
-              this.setState({
-                      articles: result.articles,  
-                      top_four: result.articles   
-                 })
-            });
-      
-          }*/
 
-          
-    render() {
-    
-        return(
-            <section id="element">
+  componentDidMount() {
+    fetch(`https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_CLIENT_ID_KEY}`)
+      .then(result => result.json())
+      .then(result => {
+        console.log(result)
+        this.setState({
+          pics: result
+          //         top_four: result.articles   
+        })
+      });
 
-          
-          <ul class="nav nav-tabs" id="myTab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-  </li>
-</ul>
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...1
-    <h5>New Image</h5>
-  </div>
-  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...2</div>
-  
-</div>
-  
-        
-</section>
-            
+  }
 
-            
-      
-            )
-        }
+
+  render() {
+
+    return (
+      <section id="element">
+
+
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+          <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...1
+          <h5>New Image</h5>
+
+          <div class="card">
+            <div class="card-body">
+              This is some text within a card body.
+            </div>
+          </div>
+
+          </div>
+          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...2</div>
+
+        </div>
+
+
+      </section>
+
+
+
+
+    )
+  }
 
 
 }
@@ -65,4 +69,3 @@ class Index extends Component {
 export default Index;
 
 
-          
