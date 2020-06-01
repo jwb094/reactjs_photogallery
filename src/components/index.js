c1b9b04730fc92b7f7099aaf276de44b585de0fb
Josh Bello <joshbello@joshs-mbp.home>
2020-06-01 21:14:55 +0100

    finished logic and panel for search photo task

--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,29 @@ class Index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pics: []
+      pics: [],
+      search_pics: [],
+      term:''
     };
+    this.searchimages = this.searchimages.bind(this);
   }
 
 
+    searchimages(){
+      this.setState({
+        term:document.getElementById('search_term').value
+      });
+
+      fetch(`https://api.unsplash.com/photos?query=?${this.state.term}&client_id=${process.env.REACT_APP_CLIENT_ID_KEY}`)
+      .then(result => result.json())
+      .then(result => {
+        console.log(result)
+        this.setState({
+          search_pics: result
+        })
+      });
+
+    }
 
 
   componentDidMount() {
@@ -36,7 +54,7 @@ class Index extends Component {
             <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Search</a>
           </li>
         </ul>
         <div class="tab-content" id="myTabContent">
@@ -58,7 +76,28 @@ class Index extends Component {
 
 
           </div>
-          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...2</div>
+          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Search Images
+          <div className="col-sm-12">
+                <div className="input-group mb-3">
+                    <input type="text" class="form-control" id="search_term" placeholder="E.g. Beach,city,dog" aria-describedby="button-addon2"></input>
+                    <div className="input-group-append">
+                            <button className="btn btn-outline-secondary" type="button" id="button-addon2"  onClick={this.searchimages}>Button</button>
+                    </div>
+                </div>
+                <h5>New Image</h5>
+          {this.state.search_pics.map(pic => 
+          
+            <div class="card">
+            <img class="card-img-top" src={pic.urls.full} alt="Card image cap"></img>
+            <div class="card-body">
+              photo by <a href={`{$pic.user.links.self}/?utm_source=your_app_name&utm_medium=referral`}> {pic.user.first_name}{pic.user.last_name} </a>
+              on <a href="https://unsplash.com/?utm_source=your_app_name&utm_medium=referral">Unsplash</a>
+            </div>
+          </div>
+          )}
+                
+            </div> 
+          </div>
 
         </div>
 
